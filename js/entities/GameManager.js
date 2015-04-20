f211a12820446df385a7a2a4e5599c524d00125d
Denis Makhlin <denismakhlin@gmail.com>
2015-04-20 15:02:36 -0700

    21st

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -112,6 +112,35 @@ game.SpendGold = Object.extend({
        game.data.buyscreen.setOpacity(0.8);
        me.game.world.addChild(game.data.buyscreen, 34);
        game.data.player.body.setVelocity(0, 0);
+       me.input.bindKey(me.input.KEY.F1, "F1", true);
+       me.input.bindKey(me.input.KEY.F2, "F2", true);
+       me.input.bindKey(me.input.KEY.F3, "F3", true);
+       me.input.bindKey(me.input.KEY.F4, "F4", true);
+       me.input.bindKey(me.input.KEY.F5, "F5", true);
+       me.input.bindKey(me.input.KEY.F6, "F6", true);
+       this.setBuyText();
+   },
+   
+   setBuyText: function(){
+        game.data.buytext = new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [game.data.pausedPos.x, game.data.pausePos.y, 10, 300, 50]);
+                        this.font = new me.Font("Arial", 26, "white");
+                        this.updateWhenPaused = true;
+                        this.alwaysUpdate = true;
+                    },
+                    
+                    draw: function(render){
+                        this.font.draw(render.getContext(), "PRESS F1-F6 TO BUY, B TO EXIT. Current Gold: " + game.data.gold, this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "Skill 1: Increase Damage | Current Level: " + game.data.skill1 + " Cost: " + ((game.data.skill1+1)*10),this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "Skill 2: Run Faster! Current Level: " + game.data.skill2 + " Cost: " + ((game.data.skill2+1)*10),this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "Skill 3: Increase Health! Current Level: " + game.data.skill3 + " Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "Q Ability: Speed Burst | Current Level: " + game.data.ability1 + " Cost: " + ((game.data.ability1+1)*10), this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "W Ability: Eat Your Creep For Health: " + game.data.ability2 + " Cost: " + ((game.data.ability2+1)*10), this.pos.x, this.pos.y);
+                        this.font.draw(render.getContext(), "E Ability: Throw Your Spear: " + game.data.ability3 + " Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y);
+                    }
+                }));  
+        me.game.world.addChild(game.data.buytext, 35);        
    },
    
    stopBuying: function(){
@@ -119,6 +148,13 @@ game.SpendGold = Object.extend({
        me.state.resume(me.state.PLAY);
        game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
        me.game.world.removeChild(game.data.buyscreen, 34);
+       me.input.unbindKey(me.input.KEY.F1, "F1", true);
+       me.input.unbindKey(me.input.KEY.F2, "F2", true);
+       me.input.unbindKey(me.input.KEY.F3, "F3", true);
+       me.input.unbindKey(me.input.KEY.F4, "F4", true);
+       me.input.unbindKey(me.input.KEY.F5, "F5", true);
+       me.input.unbindKey(me.input.KEY.F6, "F6", true);
+       me.game.world.removeChild(game.data.buytext);
    }
    
 });
